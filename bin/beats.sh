b4eaf4eca04a1c8f0c8f3a0360b9d70165ee4b11
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2021-08-08 16:30:46 -0400

    bin: update beats.sh and desk-shot

--- bin/beats.sh
+++ bin/beats.sh
@@ -9,9 +9,9 @@ read h m s <<<$(TZ=GMT-1 date "+%H %M %S")
 
 # (UTC+1seconds + (UTC+1minutes * 60) + (UTC+1hours * 3600)) / 86.4
 time=$(bc -l <<< "scale=2; ($s + ($m * 60) + ($h * 3600)) / 86.4 ")
-if (( $(echo "$time < 10" | bc -l ))); then
+if (( $(echo "$time < 10" | bc -l ) )); then
   time="00$time"
-elif (( $(echo "$time < 100" | bc -l ))); then
+elif (( $(echo "$time < 100" | bc -l ) )); then
   time="0$time"
 fi
 echo "@$time"
